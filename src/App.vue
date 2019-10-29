20972679079ad238a2d81a8b349b8952834a6936
Ashton Meuser <ashtonmeuser@gmail.com>
2019-10-29 00:00:11 -0700

    LESS variables, logo placeholder

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <img class="logo" src="https://via.placeholder.com/140x140">
     <FeedingForm/>
   </div>
 </template>
@@ -16,6 +17,19 @@ export default {
 </script>
 
 <style lang="less">
+@import "./assets/variables.less";
+
+body {
+  background-color: @body-background;
+}
 #app {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  max-width: 500px;
+  margin: 20px auto;
+  .logo {
+    align-self: center;
+  }
 }
 </style>
